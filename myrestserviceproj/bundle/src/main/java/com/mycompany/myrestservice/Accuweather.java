adc5a951e1c7e97eba119cf5073ad3210223f12f
gourivar <gouri.var@gmail.com>
2017-01-03 13:50:53 -0600

    commit for github 2
    adding comment

--- myrestserviceproj/bundle/src/main/java/com/mycompany/myrestservice/Accuweather.java
+++ myrestserviceproj/bundle/src/main/java/com/mycompany/myrestservice/Accuweather.java
@@ -2,6 +2,8 @@ package com.mycompany.myrestservice;
 
 /**
  * A simple service interface
+ adding a file comment to sync in github
+ https://github.com/gourivar/varaem
  */
  
 public interface Accuweather {
